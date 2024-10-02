06704dab18b3112c48e22adf4798c4e926c63e93
Aabid <hussain.aabid2002@gmail.com>
2024-10-02 14:14:50 +0530

    Added homepage ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import './App.css'
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Home from './pages/Home/Home';
 
 function App() {
 
   return (
     <div className='app-wrapper'>
       <Header color="light" light={true} expand="md" container="md" />
-      <div style={{ marginTop: '100px' }}>
-        Some Content
-      </div>
+      <Home />
       <Footer />
     </div>
   )
