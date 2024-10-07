506203c75def41b2272f6c0af3987420dc89f3d7
Aabid <hussain.aabid2002@gmail.com>
2024-10-07 12:13:45 +0530

    added signup functionality

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,14 +1,46 @@
-function Auth() {
+import { useEffect, useState } from "react";
+
+function Auth({ onSubmit, resetForm }) {
+
+     const [formDetials, setFormDetails] = useState({ email: '', password: '', username: '', isLoading: false });
+
+     function updateEmail(updatedEmail) {
+          setFormDetails({ ...formDetials, email: updatedEmail });
+     }
+
+     function updatePassword(updatedPassword) {
+          setFormDetails({ ...formDetials, password: updatedPassword });
+     }
+
+     function updateUsername(updatedUsername) {
+          setFormDetails({ ...formDetials, username: updatedUsername });
+     }
+
+     function onFormSubmit() {
+          setFormDetails({ ...formDetials, isLoading: true });
+          onSubmit(formDetials);
+     }
+
+     useEffect(() => {
+          setFormDetails({ email: '', password: '', username: '', isLoading: false })
+     }, [resetForm])
+
      return (
           <>
                <div className="input-group">
-                    <input type="text" className="form-control" placeholder="Username" id="loginUsername" />
+                    <input type="text" onChange={(e) => updateUsername(e.target.value)} value={formDetials.username} className="form-control" placeholder="Username" id="loginUsername" />
                </div>
                <div className="input-group">
-                    <input type="password" className="form-control" placeholder="Password" id="loginPassword" />
+                    <input type="email" onChange={(e) => updateEmail(e.target.value)} value={formDetials.email} className="form-control" id="loginUserEmail" placeholder="Email" />
                </div>
                <div className="input-group">
-                    <button className="form-control btn btn-primary">Submit</button>
+                    <input type="password" onChange={(e) => updatePassword(e.target.value)} value={formDetials.password} className="form-control" placeholder="Password" id="loginPassword" />
+               </div>
+               <div className="input-group">
+                    <button onClick={onFormSubmit} className="btn btn-primary form-control" type="button" disabled={formDetials.isLoading}>
+                         {(formDetials.isLoading) && <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>}
+                         {(formDetials.isLoading) ? 'Loading...' : 'Submit'}
+                    </button>
                </div>
           </>
      )
--- src/pages/Authentication/Signup.jsx
+++ src/pages/Authentication/Signup.jsx
@@ -3,9 +3,32 @@ import './Auth.css';
 
 
 import Auth from "../../components/Auth/Auth";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { signup } from '../../apis/fakeStoreProdApis';
+import { useState } from 'react';
+
 
 function Signup() {
+
+     const navigate = useNavigate();
+
+     const [resetSignupForm, setResetSignupForm] = useState(false);
+
+     async function onAuthFormSubmit(authArgs) {
+          try {
+               await axios.post(signup(), {
+                    username: authArgs.username,
+                    email: authArgs.email,
+                    password: authArgs.password
+               });
+               navigate('/signin');
+          } catch (error) {
+               console.log(error);
+               setResetSignupForm(true);
+          }
+     }
+
      return (
           <div className="container">
                <div className="row">
@@ -15,7 +38,11 @@ function Signup() {
                </div>
                <div className="login-wrapper" id="loginForm">
                     <h4 className="text-center">Signup</h4>
-                    <Auth />
+                    <Auth
+                         onSubmit={onAuthFormSubmit}
+                         resetForm={resetSignupForm}
+
+                    />
                     <div className="signup-btn text-center" id="showSignUpBtn">
                          <Link to="/signin">
                               Already have an Account? Signin Here
